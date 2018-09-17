b0ddfa343019e22dfee2ed3f7807cb4a457627f4
Henrique Lira da Silva <42049165+LiraRique@users.noreply.github.com>
2018-09-17 20:08:46 -0300

    Update jogovelha.py

--- SRC/jogovelha.py
+++ SRC/jogovelha.py
@@ -2,13 +2,14 @@ def inicializar():
     tab = [ ]
     for i in range(3):
         linha = [ ]
-    for j in range(3):
-        linha.append(".")
+        for j in range(3):
+            linha.append(".")
         tab.append(linha)
     return tab
 
 def main( ):
     jogo = inicializar( )
     print (jogo)
-    if __name__ == "__main__":
+    
+if __name__ == "__main__":
 main()
